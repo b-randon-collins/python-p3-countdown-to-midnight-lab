fc94fc6829fb645091e678aa1d120e56cd5c000f
Brandon Collins <brandonc503@yahoo.com>
2024-09-05 19:10:32 -0400

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,29 @@
-# your code goes here!
+import time
+
+def countdown(seconds):
+    if seconds <= 0:
+        print("HAPPY NEW YEAR!")
+        return
+    
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+        time.sleep(1)
+    
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(seconds):
+    if seconds <= 0:
+        print("HAPPY NEW YEAR!")
+        return
+    
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+        time.sleep(1)
+    
+    print("HAPPY NEW YEAR!")
+
+
+countdown(10)
+countdown_with_sleep(10)
